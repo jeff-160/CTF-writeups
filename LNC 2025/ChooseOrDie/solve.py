f436ceb3d37c86d929908eda336c8f5ba4bd8e8f
jeff160 <141229412+jeff-160@users.noreply.github.com>
2025-07-14 08:24:37 +0800

    prettier solve script

--- LNC 2025/ChooseOrDie/solve.py
+++ LNC 2025/ChooseOrDie/solve.py
@@ -1,11 +1,7 @@
 import hashlib
 import base64
 
-target_b64 = "LNSg2cOUwwiVgmq4nKdWBA=="
-target_md5 = base64.b64decode(target_b64)
-
-def md5_of_buffer(buf):
-    return hashlib.md5(buf).digest()
+target_hash = base64.b64decode("LNSg2cOUwwiVgmq4nKdWBA==")
 
 values = [6, 7]
 
@@ -15,7 +11,9 @@ for i in range(2 ** 16):
         bit = (i >> j) & 1
         buffer[j] = values[bit]
 
-    if md5_of_buffer(buffer) == target_md5:
+    input_hash = hashlib.md5(buffer).digest()
+
+    if input_hash == target_hash:
         seq = ['Yes' if b == 6 else 'No' for b in buffer]
         print("Found matching sequence!")
         print(seq)
